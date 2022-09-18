1d485b7e51bd8cb3c4c48bcdf0d979ba63eaacd8
Chris Parsons <chris@chris-parsons.com>
2022-09-17 22:27:51 -0700

    Move server file into own folder.

--- server/index.ts
+++ server/index.ts
@@ -55,8 +55,8 @@ app.disable("x-powered-by");
 
 // Remix fingerprints its assets so we can cache forever.
 app.use(
-  "/build",
-  express.static("public/build", { immutable: true, maxAge: "1y" })
+  "../build",
+  express.static("../public/build", { immutable: true, maxAge: "1y" })
 );
 
 // Everything else (like favicon.ico) is cached for an hour. You may want to be
